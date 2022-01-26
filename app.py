58b020d3af3206925b512492eea3c5aafaee1a89
cshelton7 <cshelton7@student.gsu.edu>
2022-01-26 13:19:52 -0500

    app creation

---
+++ app.py
@@ -0,0 +1,14 @@
+import flask
+
+#object in the code that is our app, declaring app
+#flask. --> coming from library
+#Flask(__name__) --> passed the name of the app
+app = flask.Flask(__name__)
+
+#tell flask what function to run when flask app is visited
+#hey flask, this function lives here!
+@app.route("/")
+
+def index():
+    return "Hello world"
+
